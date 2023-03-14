b0de73120b1728097e61b64e0448e1d2fe019ac9
hyeongki96 <hyeongki96@korea.ac.kr>
2023-03-14 14:57:15 +0900

    스프링 실습 회원가입, 로그인, 로그아웃, 회원삭제 기능 구현

--- src/main/java/com/myspring/pro27/HomeController.java
+++ src/main/java/com/myspring/pro27/HomeController.java
@@ -14,30 +14,35 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//@Controller
+//public class HomeController {
+//  private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//  /**
+//  * Simply selects the home view to render by returning its name.
+//  */
+//  @RequestMapping(value = "/", method = RequestMethod.GET)
+//  public String home(Locale locale, Model model) {
+//    logger.info("Welcome home! The client locale is {}.", locale);
+//
+//    Date date = new Date();
+//    DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, 
+//    DateFormat.LONG, locale);
+//    String formattedDate = dateFormat.format(date);
+//    model.addAttribute("serverTime", formattedDate );
+//    return "home";
+//  }
+//}
+
+
+//다국어 기능 코드
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/main.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-		
-		//첫  STS 설정
-		
-	
+		return "main";
 	}
-	
 }
+
